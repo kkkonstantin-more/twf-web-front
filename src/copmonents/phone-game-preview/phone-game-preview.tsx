7a089c3e4d6a0d0955d1617f1e905c7a183fe752
konstantin-moro <konstantinmorozovvv@gmail.com>
2020-06-11 23:23:51 +0300

    new phone-game-preview component

--- src/copmonents/phone-game-preview/phone-game-preview.tsx
+++ src/copmonents/phone-game-preview/phone-game-preview.tsx
@@ -1,36 +1,32 @@
 import React from "react";
 import { Carousel } from "react-bootstrap";
+
 import "./phone-game-preview.scss";
 
 const PhoneGamePreview: React.FC = () => {
-  const gameScreenshotsUrls: Array<string> = [
-    require("../../assets/component-phone-game-preview/screenshots/screenshot-1.jpg"),
-    require("../../assets/component-phone-game-preview/screenshots/screenshot-2.jpg"),
-    require("../../assets/component-phone-game-preview/screenshots/screenshot-3.jpg"),
-  ];
+  const gameScreenshotsUrls: Array<string> = [];
+  // filling array with urls
+  for (let i = 1; i < 12; i++) {
+    gameScreenshotsUrls.push(
+      require(`../../assets/component-phone-game-preview/screenshots/screenshot-${i}.jpg`)
+    );
+  }
 
   return (
     <div className="phone-game-preview">
-      <div className="phone-game-preview__phone-image">
-        <Carousel
-          className="phone-game-preview__screenshots-carousel"
-          indicators={false}
-          controls={false}
-          // interval between slide swaps
-          interval={3000}
-        >
-          {gameScreenshotsUrls.map((screenshotUrl: string, i: number) => (
-            <Carousel.Item key={i}>
-              <img
-                src={screenshotUrl}
-                width="100%"
-                height="auto"
-                alt={`game screenshot number ${i}`}
-              />
-            </Carousel.Item>
-          ))}
-        </Carousel>
-      </div>
+      <Carousel
+        className="phone-game-preview__screenshots-carousel"
+        indicators={false}
+        controls={false}
+        // interval between slide swaps
+        interval={3000}
+      >
+        {gameScreenshotsUrls.map((screenshotUrl: string, i: number) => (
+          <Carousel.Item key={i} className="phone-game-preview__screenshot">
+            <img src={screenshotUrl} alt={`game screenshot number ${i}`} />
+          </Carousel.Item>
+        ))}
+      </Carousel>
     </div>
   );
 };
